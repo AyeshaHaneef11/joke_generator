acbdb6453034818c720794726eee0f4d8cf23883
ayesha fatima <ayeshahaneef21@gmail.com>
2024-10-16 23:27:21 +0500

    added DOM and event listener to render the jokes on the page

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+
 const URL = 'https://official-joke-api.appspot.com/random_joke';
+const button = document.querySelector('#btn');
+const setupPara  = document.querySelector('#setUp');
+const punchlinePara = document.querySelector('#punchline');
 
 const getJoke = async ()=>{
-  console.log('fetching data...');
+  //console.log('fetching data...');
   const response = await fetch(URL); // it returns a promise
   const data = await response.json();
-  console.log(data);
-  
 
+  //console.log(data.setup);
+  setupPara.innerText = data.setup;
+  punchlinePara.innerHTML = `${data.punchline} &#128514`;
 
-   
 }
-getJoke();
+
+button.addEventListener('click' , getJoke);
+
